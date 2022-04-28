0859573861fe54c1a2bba984576b78e750ef93be
Aidan Clark <84424245+pwrac@users.noreply.github.com>
2022-04-27 21:40:45 -0600

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,11 +1,11 @@
   1 from twitchAPI.twitch import Twitch
   2 from os.path import exists
   3 
-  4 client_id = 'YOUR_CLIENT_ID'
-  5 client_secret = 'YOUR_CLIENT_SECRET'
+  4 client_id = 'YOUR CLIENT ID'
+  5 client_secret = 'YOUR CLIENT SECRET'
   6 twitch = Twitch(client_id, client_secret)
   7 
-  8 channel_name = 'qiqpip'
+  8 channel_name = 'DESIRED CHANNEL'
   9 channel_id = twitch.get_users(logins=[channel_name]).get('data')[0].get('id')
  10 
  11 curr_follows = str(twitch.get_users_follows(first=1, to_id=channel_id)['total'])
