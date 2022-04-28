c9e124a50ec809fc4759e8ef4e9a0bbf124381a7
Aidan Clark <84424245+pwrac@users.noreply.github.com>
2022-04-27 21:18:06 -0600

    Initial Commit
    Initial Commit

---
+++ tracker.py
@@ -0,0 +1,17 @@
+  1 from twitchAPI.twitch import Twitch
+  2 from os.path import exists
+  3 
+  4 client_id = 'YOUR_CLIENT_ID'
+  5 client_secret = 'YOUR_CLIENT_SECRET'
+  6 twitch = Twitch(client_id, client_secret)
+  7 
+  8 channel_name = 'qiqpip'
+  9 channel_id = twitch.get_users(logins=[channel_name]).get('data')[0].get('id')
+ 10 
+ 11 curr_follows = str(twitch.get_users_follows(first=1, to_id=channel_id)['total'])
+ 12 
+ 13 # print(f"Followers: {curr_follows} / 10")
+ 14 
+ 15 fp = open('count.txt', 'w')
+ 16 fp.write(curr_follows)
+ 17 fp.close()
