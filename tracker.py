b9d4b69f6f7c519b13b78d69f883e04928bd9955
Aidan Clark <84424245+pwrac@users.noreply.github.com>
2022-05-20 20:34:13 -0700

    Added Time Elapsed in Console; Neatness
    Adds time elapsed in H:MM:SS format in the console. Restructured for neatness.

--- tracker.py
+++ tracker.py
@@ -1,19 +1,23 @@
 from twitchAPI.twitch import Twitch
-from os.path import exists
-from time import sleep
- 
+from time import time, sleep
+from datetime import timedelta
+
 client_id = 'YOUR CLIENT ID'
 client_secret = 'YOUR CLIENT SECRET'
-twitch = Twitch(client_id, client_secret)
- 
 channel_name = 'DESIRED CHANNEL'
+
+twitch = Twitch(client_id, client_secret)
 channel_id = twitch.get_users(logins=[channel_name]).get('data')[0].get('id')
 
+start = time()
+
 while True:
+    time_elapsed = int(time() - start)
     curr_follows = str(twitch.get_users_follows(first=1, to_id=channel_id)['total'])
+   
     fp = open('count.txt', 'w')
     fp.write(curr_follows) 
     fp.close()
-    print(f"Follow count updated [Followers: {curr_follows}]")
+    
+    print(f"[{timedelta(seconds = time_elapsed)}]: Follow count updated to {curr_follows}")
     sleep(30) # Updates every 30 seconds
-
