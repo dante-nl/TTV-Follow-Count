68d7dde0123488e5cca3dec53de999a688f3d128
Aidan Clark <84424245+pwrac@users.noreply.github.com>
2022-05-20 21:14:06 -0700

    Replace Elapsed Time with Local-Time

--- tracker.py
+++ tracker.py
@@ -1,6 +1,6 @@
 from twitchAPI.twitch import Twitch
-from time import time, sleep
-from datetime import timedelta
+from time import sleep
+from datetime import datetime
 
 client_id = 'YOUR CLIENT ID'
 client_secret = 'YOUR CLIENT SECRET'
@@ -9,15 +9,13 @@ channel_name = 'DESIRED CHANNEL'
 twitch = Twitch(client_id, client_secret)
 channel_id = twitch.get_users(logins=[channel_name]).get('data')[0].get('id')
 
-start = time()
-
 while True:
-    time_elapsed = int(time() - start)
+    curr_time = datetime.now().strftime("%H:%M:%S")
     curr_follows = str(twitch.get_users_follows(first=1, to_id=channel_id)['total'])
    
     fp = open('count.txt', 'w')
     fp.write(curr_follows) 
     fp.close()
     
-    print(f"[{timedelta(seconds = time_elapsed)}]: Follow count updated to {curr_follows}")
+    print(f"[{curr_time}]: Follow count updated to {curr_follows}")
     sleep(30) # Updates every 30 seconds
