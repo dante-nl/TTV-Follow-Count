49a35e52f61dae6f64a086fe4c868aeff29320e1
Aidan C <84424245+pwrac@users.noreply.github.com>
2022-08-25 11:27:04 -0600

    Add condition for tracker to update
    Now only updates when the follow count has changed every 30 seconds.

--- tracker.py
+++ tracker.py
@@ -7,17 +7,23 @@ client_id = 'YOUR CLIENT ID'
 client_secret = 'YOUR CLIENT SECRET'
 channel_name = 'DESIRED CHANNEL'
 
-
 twitch = Twitch(client_id, client_secret)
 channel_id = twitch.get_users(logins=[channel_name]).get('data')[0].get('id')
 
+last_follows = 0
+
 while True:
-    curr_time = datetime.now().strftime("%H:%M:%S")
     curr_follows = str(twitch.get_users_follows(first=1, to_id=channel_id)['total'])
-   
-    fp = open('count.txt', 'w')
-    fp.write(curr_follows) 
-    fp.close()
     
-    print(f"[{curr_time}]: Follow count updated to {curr_follows}")
+    # Update only if follow count has changed
+    if (curr_follows != last_follows):
+        last_follows = curr_follows
+        curr_time = datetime.now().strftime("%H:%M:%S")
+        
+        fp = open('count.txt', 'w')
+        fp.write(curr_follows) 
+        fp.close()
+        
+        print(f"[{curr_time}]: Follow count updated to {curr_follows}")
+        
     sleep(30) # Updates every 30 seconds
