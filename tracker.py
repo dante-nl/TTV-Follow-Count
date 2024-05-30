12a54917650d902b3618cb2faa5fdcba999b74a4
dante_nl <61943433+dante-nl@users.noreply.github.com>
2024-05-30 07:49:42 +0200

    Made it so it works, without leaking personal data
    I've fixed the Python program so it works again with the latest versions of the Twitch API and the twitchAPI library (v4.2.0)

--- tracker.py
+++ tracker.py
@@ -1,29 +1,47 @@
-from twitchAPI.twitch import Twitch
-from time import sleep
 from datetime import datetime
+import time
+from twitchAPI.helper import first
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticationStorageHelper
+from twitchAPI.type import AuthScope
+import asyncio
 
 ### ⬇️ ENTER YOUR INFO HERE ⬇️ ###
-client_id = 'YOUR CLIENT ID'
-client_secret = 'YOUR CLIENT SECRET'
-channel_name = 'DESIRED CHANNEL'
+client_id = 'YOUR CLIENT ID HERE'
+client_secret = 'YOUR CLIENT SECRET HERE'
 
-twitch = Twitch(client_id, client_secret)
-channel_id = twitch.get_users(logins=[channel_name]).get('data')[0].get('id')
 
-last_follows = 0
+TARGET_SCOPES = [AuthScope.MODERATOR_READ_FOLLOWERS]
 
-while True:
-    curr_follows = str(twitch.get_users_follows(first=1, to_id=channel_id)['total'])
-    
-    # Update only if follow count has changed
-    if (curr_follows != last_follows):
-        last_follows = curr_follows
-        curr_time = datetime.now().strftime("%H:%M:%S")
-        
-        fp = open('count.txt', 'w')
-        fp.write(curr_follows) 
-        fp.close()
-        
-        print(f"[{curr_time}]: Follow count updated to {curr_follows}")
-        
-    sleep(30) # Updates every 30 seconds
+
+async def run():
+	# create the api instance and get user auth either from storage or website
+	twitch = await Twitch(client_id, client_secret)
+	helper = UserAuthenticationStorageHelper(twitch, TARGET_SCOPES)
+	await helper.bind()
+
+	# get the currently logged in user
+	user = await first(twitch.get_users())
+
+	# make a loop, that can be quit with ctrl+c
+	try:
+		while True:
+			# get the followers
+			followers = await twitch.get_channel_followers(user.id)
+			with open("count.txt", "w") as follower_file:
+				# save to file
+				follower_file.write(str(followers.total))
+
+			# logging
+			curr_time = datetime.now().strftime("%H:%M:%S")
+			print(f"[{curr_time}]: Follow count updated to {followers.total}")
+			# and do it all again in 30 seconds
+			time.sleep(30)
+	except KeyboardInterrupt:
+		pass
+	finally:
+		# stopping both eventsub as well as gracefully closing the connection to the API
+		await twitch.close()
+
+
+asyncio.run(run())
